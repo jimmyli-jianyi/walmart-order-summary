1fca3194981b6a222a54e709b703ff5b3bf885b3
Jimmy Li <jimmy.jianyil@gmail.com>
2018-10-25 03:15:32 -0700

    improved UI

--- src/components/DetailExpander.js
+++ src/components/DetailExpander.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import './DetailExpander.css';
 
 export default class DetailExpander extends Component {
   render() {
     const itemDetails = this.props.items.map((item, idx) => (
-      <div key={idx}>
-        <img src={item.img} alt="imgURL" />
+      <div className="itemDetail" key={idx}>
+        <div className="itemImg">
+          <img src={item.img} alt="imgURL" />
+        </div>
+        <div className="itemDescription">
+          <span className="itemName">{item.itemName}</span>
+          <span className="itemPrice">
+            <b>${item.price}</b>
+          </span>
+          <span className="itemColor">color: {item.color}</span>
+        </div>
       </div>
     ));
 
